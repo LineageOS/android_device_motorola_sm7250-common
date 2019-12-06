b38b85c7dd663a93610df2fb6b308e5f88a547ee
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 13:19:19 -0800

    sm7250-common: Setup 6GB Dalvik heap configuration
    Change-Id: Ia2b826c95643f935878335c2434deadf7f8401c4

--- common.mk
+++ common.mk
@@ -20,6 +20,9 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
