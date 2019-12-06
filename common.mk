d96e0ac1b6beb416ee932e04dcda20fa038c5900
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 13:19:19 -0800

    sm7250-common: Setup 6GB Dalvik heap configuration
    Change-Id: Ia2b826c95643f935878335c2434deadf7f8401c4

--- common.mk
+++ common.mk
@@ -17,6 +17,9 @@
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
