100a469a7585a7ef3b2cd8f9c19e279f9f106d3d
Andrew Hexen <SyberHexen@gmail.com>
2021-07-01 12:15:52 -0700

    sm7250-common: dtc: specify we are using prebuilt.
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: Icf98eeee9069407466b408c22c64ef43b435baa1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,6 +60,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/motorola/sm7250
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_ADDITIONAL_FLAGS += \
+    DTC_PREBUILT=true \
     DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
     DTC_OVERLAY_TEST_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/ufdt_apply_overlay \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
