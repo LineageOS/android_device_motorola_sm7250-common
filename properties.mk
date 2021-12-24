b137f808f9e049abc9731d75808ac6a41e570f6e
Andrew Hexen <SyberHexen@gmail.com>
2021-12-24 01:27:53 -0800

    sm7250-common: fix voice.path.for.pcm.voip
    * in change 'I3814b8b30f9a5566ebc8c8b7c6b936def7649058' this was set false but in our stock for sm7250 the logic should be set for true in our case..
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: Ia274612a93badcd21fcd190f0cce33ad8466e60e

--- properties.mk
+++ properties.mk
@@ -38,10 +38,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio_hal.in_period_size=144 \
     vendor.audio_hal.period_multiplier=3 \
     vendor.audio_hal.period_size=192 \
-    vendor.voice.path.for.pcm.voip=false
+    vendor.voice.path.for.pcm.voip=true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    use.voice.path.for.pcm.voip=false
+    use.voice.path.for.pcm.voip=true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     audio.offload.min.duration.secs=60 \
