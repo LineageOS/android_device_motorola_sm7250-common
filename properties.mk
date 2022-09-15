ee5b2468ea64e81e10e8bb19a6882c765f3fa172
Andrew Hexen <SyberHexen@gmail.com>
2022-09-15 14:49:44 -0700

    sm7250-common: Set correct props with AOSP Bluetooth
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I5e69beb585a3643e44afe97c0f8d37c41f9a787d

--- properties.mk
+++ properties.mk
@@ -94,8 +94,9 @@ PRODUCT_ODM_PROPERTIES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.bluetooth.a2dp_offload.disabled=false \
+    persist.bluetooth.a2dp_offload.cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
     persist.vendor.qcom.bluetooth.enable.splita2dp=true \
-    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
     persist.vendor.qcom.bluetooth.soc=cherokee \
     ro.bluetooth.a2dp_offload.supported=true \
     vendor.qcom.bluetooth.soc=cherokee
