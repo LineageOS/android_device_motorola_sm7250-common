6745cf44c921b330bfd9e55d823c2b4d6a8e9502
Andrew Hexen <SyberHexen@gmail.com>
2022-10-13 05:21:06 -0700

    sm7250-common: transition to smith vendor 'S2PS32.57-23-21'
    'motorola/smith_retail/smith:11/S2PS32.57-23-21/c382c9:user/release-keys'
    
    * LA.UM.9.12.r1-13500.01-SMxx50.QSSI12.0
    * LA.QSSI.11.0.r1-13000-qssi.0
    
    Co-authored-by: Vivekachooz <vivekachooz@gmail.com>
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I16b74710101d2a6ff982375b2de6db814227a7eb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -233,7 +233,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_RIL_VARIANT := caf
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-10-01
+VENDOR_SECURITY_PATCH := 2022-08-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
