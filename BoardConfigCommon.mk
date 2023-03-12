3bd7601a660549d819a7079f94d71bb8c978e82f
electimon <electimon@gmail.com>
2023-03-12 03:07:49 +0000

    sm7250-common: Remove unneeded cmdline property
    * RTB filter is a debug property.
    
    Change-Id: If5347a00492c6782f17355b5221c033987475b09
    Signed-off-by: electimon <electimon@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,7 +43,6 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.console=ttyMSM0 \
     androidboot.memcg=1 \
     lpm_levels.sleep_disabled=1 \
-    msm_rtb.filter=0x237 \
     service_locator.enable=1 \
     androidboot.usbcontroller=a600000.dwc3 \
     swiotlb=2048 \
