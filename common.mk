85663030f5226a772e1628b36aa10f567a5b09dc
Aaron Kling <webgeek1234@gmail.com>
2023-08-25 15:29:05 -0500

    sm7250-common: Update proprietary files to resolve missing deps
    Also remove several unused prebuilts and a few conflicts with source
    built targets
    
    Change-Id: I721410529397acfe492e642636bd314b7983a6ad

--- common.mk
+++ common.mk
@@ -426,6 +426,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
     libprotobuf-cpp-full \
+    libqsap_sdk \
     librmnetctl \
     libxml2
 
