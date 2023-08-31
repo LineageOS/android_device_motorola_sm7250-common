a9a7dd0479df91d8c37bc37cf5942c85334c8664
Aaron Kling <webgeek1234@gmail.com>
2023-08-31 13:54:32 -0500

    sm7250-common: Add vendor soong imports to extract
    Change-Id: Icfc50406b7a704df56f8883f1423050f9996ec47

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -20,6 +20,17 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat << EOF >> "$1"
+		"device/motorola/sm7250-common",
+		"hardware/qcom-caf/sm8250",
+		"hardware/qcom-caf/wlan",
+		"vendor/qcom/opensource/commonsys/display",
+		"vendor/qcom/opensource/dataservices",
+		"vendor/qcom/opensource/display",
+EOF
+}
+
 # Initialize the helper for common
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
@@ -34,6 +45,7 @@ write_footers
 
 if [ -s "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
+    source "${MY_DIR}/../../${VENDOR}/${DEVICE}/setup-makefiles.sh"
     setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
 
     # Warning headers and guards
