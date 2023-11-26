ad1b0faf4fd3b1c315588ac1952b3f6637d061d1
Andrew Hexen <SyberHexen@gmail.com>
2023-11-26 06:10:44 -0800

    sm7250-common: rootdir: fix init.qti.chg_policy.sh
    * Add device check for smith to restore logic for other devices to fix battery stats / fuelgauge.
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: Ia28741d439007adebe36218488555c909e4e4387

--- rootdir/bin/init.qti.chg_policy.sh
+++ rootdir/bin/init.qti.chg_policy.sh
@@ -10,20 +10,26 @@
 
 export PATH=/vendor/bin
 
+device=`getprop ro.vendor.hw.device`
 prefix="/sys/class/"
 #List of folder for ownership update
 arr=( "power_supply/battery/" "power_supply/usb/" "power_supply/main/" "power_supply/charge_pump_master/" "power_supply/pc_port/" "power_supply/dc/" "power_supply/bms/" "power_supply/parallel/" "usbpd/usbpd0/" "qc-vdm/" "charge_pump/" "qcom-battery/" )
 for i in "${arr[@]}"
 do
     for j in `ls "$prefix""$i"`
-    do
+	do
         #skip directories to prevent possible security issues.
         if [[ -d "$prefix""$i""$j" ]]
         then
             continue
         else
             chown -h system.system "$prefix""$i""$j"
-        fi
-    done
+		fi
+	done
 done
-setprop persist.vendor.hvdcp_opti.start 0
+
+if [ "$device" -eq smith ]; then
+    setprop persist.vendor.hvdcp_opti.start 0
+else
+    setprop persist.vendor.hvdcp_opti.start 1
+fi
