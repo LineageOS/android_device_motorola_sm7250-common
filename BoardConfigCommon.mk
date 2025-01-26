5a301eef5fe876c96cacef734a54b4cbe75c0ef3
Han Sol Jin <hansol@hansol.ca>
2025-01-26 18:29:48 +0700

    sm7250-common: Switch to minimal product partition size
    Our system partition is too small to fit the full thing.
    
    Change-Id: I528225a4d0a39ce23b79fb35e9442f625ec43ee4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -110,6 +110,7 @@ TARGET_MODULE_ALIASES += \
 TARGET_USES_ION := true
 
 # Partitions
+BOARD_PRODUCTIMAGE_MINIMAL_PARTITION_RESERVED_SIZE := true
 -include vendor/lineage/config/BoardConfigReservedSize.mk
 
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
