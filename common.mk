302c2ff789cac4f9943d3bdba141a4fd2597c757
Bruno Martins <bgcngm@gmail.com>
2025-02-06 21:32:31 +0000

    sm7250-common: Use aidl sensors multihal
    Change-Id: I21da3da39e9726dca693d6bcbe42e93be861f7c7

--- common.mk
+++ common.mk
@@ -358,7 +358,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal
+    android.hardware.sensors-service.multihal
 
 # Shims
 PRODUCT_PACKAGES += \
