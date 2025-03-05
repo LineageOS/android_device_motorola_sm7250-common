1038acbd4e0043a89ebad8d9d987a38a8c7a0cdc
Wiktor Rudzki <zlewchan@outlook.com>
2025-03-05 10:14:45 +0530

    sm7250-common: Commonize C2 blobs and configs
    Kang them from nairo, because they're missing from smith dump.
    They don't differ between devices.
    
    Change-Id: I568ca621fc9fcad70b6560ce9498d43fcd26e41d

--- common.mk
+++ common.mk
@@ -277,6 +277,10 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml
+
+PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml
