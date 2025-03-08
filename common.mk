c0605480eee4f4301b7f903e8bd1a5170197da39
basamaryan <basam.aryan@gmail.com>
2025-03-08 01:09:34 -0500

    sm7250-common: Move USB PRODUCT_SOONG_NAMESPACES import
    Change-Id: Ie6076ffe65f9d2384870a9726f6c8260537e5a31

--- common.mk
+++ common.mk
@@ -371,7 +371,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
-    hardware/qcom-caf/common/libqti-perfd-client
+    hardware/qcom-caf/common/libqti-perfd-client \
+    vendor/qcom/opensource/usb/etc
 
 # Telephony
 PRODUCT_PACKAGES += \
@@ -405,8 +406,6 @@ PRODUCT_PACKAGES += \
     android.hardware.usb-service.qti \
     init.qcom.usb.rc \
     init.qcom.usb.sh
-
-PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
 
 # VNDK
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := strict
