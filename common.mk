40492b4ff30d65e33244606ae651d4f6bd2c995c
basamaryan <basam.aryan@gmail.com>
2025-03-08 15:21:55 -0500

    sm7250-common: Drop (probably) unused packages
    Change-Id: I35661111d206c23a874a33e2565cb43a80290be6

--- common.mk
+++ common.mk
@@ -146,9 +146,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    libcamera2ndk_vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor \
-    android.frameworks.displayservice@1.0.vendor
+    libcamera2ndk_vendor
 
 # Common init scripts
 PRODUCT_PACKAGES += \
@@ -376,10 +374,6 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
-    vendor.qti.hardware.wifi.hostapd@1.0.vendor \
-    vendor.qti.hardware.wifi.hostapd@1.1.vendor \
-    vendor.qti.hardware.wifi.supplicant@2.0.vendor \
-    vendor.qti.hardware.wifi.supplicant@2.1.vendor \
     WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
