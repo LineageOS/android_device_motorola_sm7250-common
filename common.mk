df778314b721e0297d3f01221ab151d430a506d6
Michael Bestas <mkbestas@gmail.com>
2025-03-08 21:43:32 +0200

    sm7250-common: Enable ELF checks for libwpa_client dependent libs
    Change-Id: I490df02b68da4b9a7088a239672a1dcace66d270

--- common.mk
+++ common.mk
@@ -337,7 +337,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
-    libwpa_client \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
     WifiOverlay \
