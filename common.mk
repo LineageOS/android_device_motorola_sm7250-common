2a4e514c9a02f778282223681313a9b8855600b4
basamaryan <basam.aryan@gmail.com>
2025-03-08 00:21:48 -0500

    sm7250-common: Remove non-existant properties.mk include
    Change-Id: Ife4173129bd74cae14305e4f065778f3b131d1dd

--- common.mk
+++ common.mk
@@ -66,9 +66,6 @@ PRODUCT_PACKAGES += \
     tune2fs_ramdisk \
     resize2fs_ramdisk
 
-# Properties
--include $(LOCAL_PATH)/properties.mk
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
