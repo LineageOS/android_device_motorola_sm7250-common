f39786e30b46cad99de2f9206cf1f35f2652b949
Michael Bestas <mkbestas@lineageos.org>
2025-03-11 02:17:16 +0200

    sm7250-common: Add hardware/motorola to soong namespaces
    Change-Id: I1606d2dc9cef82a4ce9d8764c1f52b59d183e9b2

--- common.mk
+++ common.mk
@@ -299,6 +299,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
+    hardware/motorola \
     hardware/qcom-caf/common/libqti-perfd-client \
     vendor/qcom/opensource/usb/etc
 
