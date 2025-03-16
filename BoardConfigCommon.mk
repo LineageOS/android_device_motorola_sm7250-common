192d2a286506ef7775e64f7094e6b20681ba3114
basamaryan <basam.aryan@gmail.com>
2025-03-15 21:46:41 -0400

    sm7250-common: Drop TARGET_KERNEL_ADDITIONAL_FLAGS
    Change-Id: If6eec68d1622c62a0132254ed29f6d25f9f1cd6e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,11 +55,6 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/motorola/sm7250
-TARGET_KERNEL_ADDITIONAL_FLAGS += \
-    DTC_PREBUILT=true \
-    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
-    DTC_OVERLAY_TEST_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/ufdt_apply_overlay \
-    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 
 # Kernel modules - Audio
 TARGET_MODULE_ALIASES += \
