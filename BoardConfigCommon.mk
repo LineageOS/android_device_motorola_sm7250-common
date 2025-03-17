9942437e249380082c6369d685824c3ecdc96a70
basamaryan <basam.aryan@gmail.com>
2025-03-17 17:10:27 -0400

    sm7250-common: Inherit from common motorola FCM
    Change-Id: I6f2c3d47541480b9fe05ce749e7c4cc01a8a3256

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,7 +116,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/framework_compatibility_matrix.xml \
+    hardware/motorola/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/lineage/config/device_framework_matrix.xml
