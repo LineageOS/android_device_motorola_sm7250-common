96ff5fb60828cb3517d40f22d4883eaf270b5102
basamaryan <basam.aryan@gmail.com>
2025-03-21 20:42:35 -0400

    sm7250-common: Import init.mmi.charge_only.rc
    Change-Id: I1ef7ab04413c5af13f6941ef88ff6b7c5944c254

--- common.mk
+++ common.mk
@@ -154,6 +154,7 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.crda.sh \
     init.mdm.sh \
+    init.mmi.charge_only.rc \
     init.mmi.rc \
     init.mmi.touch.sh \
     init.oem.hw.sh \
