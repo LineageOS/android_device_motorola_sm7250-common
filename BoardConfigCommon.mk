PixelOS-Devices/device_motorola_sm7250-common

sm7250-common: Add system_ext to dynamic partitions list
Change-Id: Ibac5e64c748fdbe2d26acb8fcaa85c674d3ba92f

BoardConfigCommon.mk
@@ -143,13 +143,15 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 104857600
 BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_MOTOROLA_DYNAMIC_PARTITIONS_PARTITION_LIST := product system vendor
+BOARD_MOTOROLA_DYNAMIC_PARTITIONS_PARTITION_LIST := product system vendor system_ext
 BOARD_MOTOROLA_DYNAMIC_PARTITIONS_SIZE ?= 5268184832
 BOARD_SUPER_PARTITION_SIZE ?= 10736369664
 BOARD_SUPER_PARTITION_GROUPS := motorola_dynamic_partitions
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 TARGET_COPY_OUT_PRODUCT := product
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_USERIMAGES_USE_F2FS := true
 

LineageOS/android_device_motorola_sm7250-common

BoardConfigCommon.mk
@@ -142,13 +142,15 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 104857600
 BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_MOTOROLA_DYNAMIC_PARTITIONS_PARTITION_LIST := product system vendor
+BOARD_MOTOROLA_DYNAMIC_PARTITIONS_PARTITION_LIST := product system system_ext vendor
 BOARD_MOTOROLA_DYNAMIC_PARTITIONS_SIZE ?= 5363990528 # (BOARD_SUPER_PARTITION_SIZE / 2) - 4MB
 BOARD_SUPER_PARTITION_SIZE ?= 10736369664
 BOARD_SUPER_PARTITION_GROUPS := motorola_dynamic_partitions
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 TARGET_COPY_OUT_PRODUCT := product
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_USERIMAGES_USE_F2FS := true
 