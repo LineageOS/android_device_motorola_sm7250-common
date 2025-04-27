5d886a72af319c3bf9e3b9b12764af97216c14f3
Giovanni Ricca <giovanniricca@proton.me>
2025-04-27 10:16:15 +0200

    sm7250-common: Drop libinit
    Change-Id: Ided258b3d7a1423e97d8ccc15dfdca2353a50079

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,10 +126,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_lito
-TARGET_RECOVERY_DEVICE_MODULES := libinit_lito
-
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/power_supply/qcom_battery/battery_charging_enabled
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := true
