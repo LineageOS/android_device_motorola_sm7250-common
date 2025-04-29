6b5af26b1c28a96877a0f63346b44e6151a1a55a
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    sm7250-common: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- common.mk
+++ common.mk
@@ -172,6 +172,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/qcom_battery/battery_charging_enabled)
+$(call soong_config_set,lineage_health,charging_control_supports_toggle,true)
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
