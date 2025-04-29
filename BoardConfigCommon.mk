4add800c070e7f6d4ed6ea43adf40dad5d4f350b
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:21:39 +0100

    sm7250-common: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,9 +49,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_lito
-
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
