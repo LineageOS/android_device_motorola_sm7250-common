343df9e6f18c0d146691802c54c3f4bc584d2709
basamaryan <basam.aryan@gmail.com>
2025-05-05 19:41:51 -0400

    sm7250-common: Don't inherit legacy QTI FCM
    Change-Id: I6d1569e6dcd32a27ab92ad5db616db6eb984e181

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -119,7 +119,6 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/motorola/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
