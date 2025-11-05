166403be22f549b07a1c4c0f40f1d80cdf34b4fb
Giovanni Ricca <giovanniricca@proton.me>
2025-11-05 23:39:51 +0100

    sm7250-common: Enable zero packet check on fastbootd
    Change-Id: Ifad3f67cc445bd4418a8d2d7ee62ee118cc60a62

--- common.mk
+++ common.mk
@@ -109,6 +109,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+$(call soong_config_set_bool,fastbootd,zero_packet,true)
+
 # Framework detect
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor # Needed by CNE app
