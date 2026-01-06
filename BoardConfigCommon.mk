1555b6546acf5905fe922607d099688c918dcdac
Giovanni Ricca <giovanniricca@proton.me>
2026-01-06 14:44:26 +0100

    sm7250-common: Inherit motorola sm8250 kernel
    Change-Id: I9815e37d9d33d4edfe444b76fd01818aae68592f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,7 +66,10 @@ BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-TARGET_KERNEL_SOURCE := kernel/motorola/sm7250
+TARGET_KERNEL_SOURCE := kernel/motorola/sm8250
+TARGET_KERNEL_CONFIG := \
+    vendor/lito-perf_defconfig \
+    vendor/ext_config/moto-lito.config vendor/ext_config/$(PRODUCT_DEVICE)-default.config
 
 # Kernel modules - Audio
 TARGET_MODULE_ALIASES += \
